8daa35317773f1f274027db60d55b38dfd611b37
Diego Matthews <diegomatthews9@gmail.com>
2019-07-26 21:31:14 -0700

    Decent progress on bio styling and some transitioning between pages :)

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,4 +1,16 @@
+// window fade in
 
+$(window).on("load", function () {
+    console.log("window ready");
+    $("body").css({
+        opacity: "1",
+        transition: "800ms"
+    });
+});
+
+// hopefully some animation between pages here
+
+// page navigation
 $(".links").on("click", function (e) {
     e.preventDefault();
 
@@ -12,4 +24,19 @@ $(".links").on("click", function (e) {
 
 
     $(this).parents().find("nav").removeClass().addClass(classActivate);
-})
+});
+
+$("#next-btn").on("click", function (e) {
+    e.preventDefault();
+
+    let activate = $(this).attr("href");
+    console.log(activate + " page is now active");
+    let classActivate = activate.substr(1, activate.length);
+    console.log(classActivate);
+
+    $(this).parents().find(".active-page").addClass("hidden-page").removeClass("active-page");
+    $(activate).removeClass("hidden-page").addClass("active-page");
+
+
+    $(this).parents().find("nav").removeClass().addClass(classActivate);
+});
